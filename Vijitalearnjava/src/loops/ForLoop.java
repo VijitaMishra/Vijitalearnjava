c134d13be282d8007ae95bdb50626c18f3093ecc
VijitaMishra <vijita.zebra@gmail.com>
2026-03-07 11:36:08 -0500

    loops

---
+++ Vijitalearnjava/src/loops/ForLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class ForLoop {
+	
+	public static void main(String[]args) {
+	
+	for(int i=0;i < 7;i++) {
+		System.out.println("value of i = "+ i);
+		
+		if(i==5) {
+			System.out.println("Number is 5");
+		}
+		}
+	}
+}
